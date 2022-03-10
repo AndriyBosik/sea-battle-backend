822e4628431d736e1e19a9bac8487d69d70de7b9
AndriiBosyk <andriybosik2002@ukr.net>
2022-03-10 10:34:38 +0200

    Fix endpoint

--- src/main/java/com/example/seabattle/controller/UserController.java
+++ src/main/java/com/example/seabattle/controller/UserController.java
@@ -5,16 +5,18 @@ import com.example.seabattle.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/users/{nickname}")
+    @GetMapping("/{nickname}")
     public Optional<User> findByNickname(@PathVariable String nickname) {
         return userService.findByNickname(nickname);
     }
