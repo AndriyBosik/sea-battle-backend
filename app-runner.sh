c2d19cd165ca811748a976584348058558eb3a7a
AndriiBosyk <andriybosik2002@ukr.net>
2022-04-06 01:08:07 +0300

    Improve bash-script to run backend

--- app-runner.sh
+++ app-runner.sh
@@ -6,10 +6,25 @@ app_name=$2
 lines=(`cat "$env_file"`)
 result=""
 
+old_ifs=$IFS
+IFS='='
+
 for param in "${lines[@]}"
 do
-  result="$result -D$param"
+  if [[ $param =~ "=" ]]; then
+    read -a strarr <<< "$param"
+    key=${strarr[0]}
+    value=${strarr[1]}
+    if [[ $param =~ " " ]]; then
+      java_param="$key=\"$value\""
+    else
+      java_param="$key=$value"
+    fi
+    result="$result -D$java_param"
+  fi
 done
+
+IFS=$old_ifs
 
 result="${result:1}"
 
